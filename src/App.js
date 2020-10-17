0bf9a08bf5d53a7f716a2b51be9025333c78813e
Salah Eddine Lalami <lalami.sdn@gmail.com>
2020-10-18 00:41:54 +0100

    add carco & carco less package

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => (
           alt="Ant Design"
         />
         <Title level={2} style={{ marginBottom: 0 }}>
-          Ant Design
+          Ant Design{" "}
         </Title>
       </Space>
     </section>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
