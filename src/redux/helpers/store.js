f8ea336b2727ebfa7a7d8e62e3fc51be3609b584
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-03-21 11:31:48 +0100

    fix login axios instance

--- src/redux/helpers/store.js
+++ src/redux/helpers/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
-import rootReducer from "../reducers";
+// import { createStore, applyMiddleware } from "redux";
+// import thunkMiddleware from "redux-thunk";
+// import { createLogger } from "redux-logger";
+// import rootReducer from "../reducers";
 
-const loggerMiddleware = createLogger();
+// const loggerMiddleware = createLogger();
 
-export const store = createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware, loggerMiddleware)
-);
+// export const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunkMiddleware, loggerMiddleware)
+// );
