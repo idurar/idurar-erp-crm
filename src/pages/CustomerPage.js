08370e512fa17aa8e554b22a8ef72db58af811f8
Mohamed Iheb Hichri <ihebhichri12@gmail.com>
2021-03-26 17:36:30 +0100

    add the dropwdown with the functions

--- src/pages/CustomerPage.js
+++ src/pages/CustomerPage.js
@@ -1,16 +1,40 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import DashboardLayout from "./layout/DashboardLayout";
-import { Layout, Breadcrumb } from "antd";
-import { DatePicker, TimePicker, Calendar } from "../antdcomponents";
-import DataTable from "../components/DataTable";
-import format from "dayjs";
+import { Dropdown, Layout, Menu } from "antd";
+import { MoreOutlined, EyeOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons"
 import CustomerTable from "../components/customerTable";
 
-
 const { Content } = Layout;
-const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
+const menu = row => {
+
+  function Show(){
+    console.log(row._id)
+  }
+  function Edit(){
+    console.log(row._id)
+  }
+  function Delete(){
+    console.log(row._id)
+  }
+
+  return(
+    <Menu>
+    <Menu.Item icon={<EyeOutlined />} onClick={Show}>
+      Show
+    </Menu.Item>
+    <Menu.Item icon={<EditOutlined />} onClick={Edit}>
+      Edit
+    </Menu.Item>
+    <Menu.Item icon={<DeleteOutlined />} onClick={Delete}>
+      Delete
+    </Menu.Item>
+  </Menu>
+  )
+}
+
 function CustomerPage() {
-  const target = "client";
+  const entity = "client";
   const columns = [
     {
       title: "Company",
@@ -28,13 +52,21 @@ function CustomerPage() {
       title: "Email",
       dataIndex: "email",
     },
+    {
+      title: "Action",
+      render : row => (
+        <Dropdown overlay={menu(row)} >
+          <MoreOutlined  style={{cursor: "pointer"}}/>
+        </Dropdown>
+      )
+    }
   ];
+
   return (
     <DashboardLayout
       contentLayout={
         <Content style={{ margin: "0 16px" }}>
-          {/* <DataTable target={target} columns={columns} /> */}
-          <CustomerTable columns={columns} />
+          <CustomerTable entity={entity} columns={columns} />
         </Content>
       }
     />
