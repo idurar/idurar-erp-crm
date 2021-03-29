85206d10c1724969d4977b7a386d9f55d420989b
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-03-29 02:59:38 +0100

    new router folder

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./utils/PrivateRoute";
-import PublicRoute from "./utils/PublicRoute";
-
-import NotFound from "./pages/NotFound";
-import Dashboard from "./pages/Dashboard";
-import Days from "./pages/Days";
-import Customer from "./pages/Customer";
-import Patient from "./pages/Patient";
-import AntdForm from "./pages/AntdForm";
-import Login from "./pages/Login";
-import Logout from "./pages/Logout";
-import "./App.less";
+import Router from "@/router";
+import { Router as RouterHistory } from "react-router-dom";
+import history from "./utils/history";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
-  // const [userData, setUserData] = useState({
-  //   token: undefined,
-  //   user: undefined,
-  // });
   return (
-    <Switch>
-      <PrivateRoute path="/" component={Dashboard} exact />
-      <PrivateRoute component={Customer} path="/customer" exact />
-      <PrivateRoute component={Patient} path="/patient" exact />
-      <PrivateRoute component={Days} path="/days" exact />
-      <PrivateRoute component={AntdForm} path="/antd" exact />
-      <PrivateRoute component={Logout} path="/logout" exact />
-      <PublicRoute component={Login} path="/login" exact />
-      <Route
-        path="*"
-        component={NotFound}
-        render={() => <Redirect to="/notfound" />}
-      />
-    </Switch>
+    <RouterHistory history={history}>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </RouterHistory>
   );
 }
 
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
+
+import NotFound from "@/pages/NotFound";
+import Dashboard from "@/pages/Dashboard";
+import Days from "@/pages/Days";
+import Customer from "@/pages/Customer";
+import Patient from "@/pages/Patient";
+import AntdForm from "@/pages/AntdForm";
+import Login from "@/pages/Login";
+import Logout from "@/pages/Logout";
+
+function Router() {
+  return (
+    <Switch>
+      <PrivateRoute path="/" component={Dashboard} exact />
+      <PrivateRoute component={Customer} path="/customer" exact />
+      <PrivateRoute component={Patient} path="/patient" exact />
+      <PrivateRoute component={Days} path="/days" exact />
+      <PrivateRoute component={AntdForm} path="/antd" exact />
+      <PrivateRoute component={Logout} path="/logout" exact />
+      <PublicRoute component={Login} path="/login" exact />
+      <Route
+        path="*"
+        component={NotFound}
+        render={() => <Redirect to="/notfound" />}
+      />
+    </Switch>
+  );
+}
+
+export default Router;
