8949f27cc95d1160629dc4bb7296029d3b34b82d
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-05-31 07:26:54 +0100

    add https to config file

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://idurarapi.herokuapp.com/api/";
+export const API_BASE_URL = "https://idurarapi.herokuapp.com/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token";
-export const DOWNLOAD_BASE_URL = "http://idurarapi.herokuapp.com/download/";
+export const DOWNLOAD_BASE_URL = "https://idurarapi.herokuapp.com/download/";
