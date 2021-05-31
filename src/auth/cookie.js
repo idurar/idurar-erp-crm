e3bba589d7b27a7ef0e79ba3bee719ab7b9228ca
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-05-31 13:05:00 +0100

    refactory to jsx

--- src/auth/cookie.js
+++ src/auth/cookie.js
@@ -1,8 +1,8 @@
 // export function getCookie(cookieName) {
-//   var name = cookieName + "=";
-//   var ca = document.cookie.split(";");
-//   for (var i = 0; i < ca.length; i++) {
-//     var c = ca[i];
+//   let name = cookieName + "=";
+//   let ca = document.cookie.split(";");
+//   for (let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
 //     while (c.charAt(0)===" ") {
 //       c = c.substring(1);
 //     }
@@ -14,9 +14,9 @@
 // }
 
 // export function setCookie(cookieName, cookieValue, expireDays = 1) {
-//   var d = new Date();
+//   let d = new Date();
 //   d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
-//   var expires = "expires=" + d.toUTCString();
+//   let expires = "expires=" + d.toUTCString();
 //   document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
 // }
 
