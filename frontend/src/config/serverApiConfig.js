b05e7202a6621bf6a129de04a73b23c15a899378
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-11-07 01:47:27 +0100

    revert config file

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,7 +1,7 @@
-export const API_BASE_URL = "http://localhost:8888/api/";
-export const ACCESS_TOKEN_NAME = "x-auth-token";
-export const DOWNLOAD_BASE_URL = "http://localhost:8888/download/";
-// export const API_BASE_URL = "https://idurar-crm-erp.herokuapp.com/api/";
+// export const API_BASE_URL = "http://localhost:8888/api/";
 // export const ACCESS_TOKEN_NAME = "x-auth-token";
-// export const DOWNLOAD_BASE_URL =
-//   "https://idurar-crm-erp.herokuapp.com/download/";
+// export const DOWNLOAD_BASE_URL = "http://localhost:8888/download/";
+export const API_BASE_URL = "https://idurar-crm-erp.herokuapp.com/api/";
+export const ACCESS_TOKEN_NAME = "x-auth-token";
+export const DOWNLOAD_BASE_URL =
+  "https://idurar-crm-erp.herokuapp.com/download/";
