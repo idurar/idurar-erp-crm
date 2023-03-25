12f1c03fdc0bee7cce033682c934799340d0bc14
idurar <lalami.sdn@gmail.com>
2023-03-25 11:49:45 +0100

    update config file

--- frontends/erpFrontend/src/config/serverApiConfig.js
+++ frontends/erpFrontend/src/config/serverApiConfig.js
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://idurar-crm-erp.herokuapp.com/api/'
+    ? 'https://calm-ruby-barracuda-sock.cyclic.app/api/'
     : 'http://localhost:8888/api/';
 export const DOWNLOAD_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://idurar-crm-erp.herokuapp.com/download/'
+    ? 'https://calm-ruby-barracuda-sock.cyclic.app/download/'
     : 'http://localhost:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
