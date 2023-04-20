067103e63be4a4efc3c7031f35dd75f9a9c23bc6
idurar <lalami.sdn@gmail.com>
2023-04-20 17:04:36 +0100

    fix bugs

--- frontends/erpFrontend/src/pages/Settings/Settings.jsx
+++ frontends/erpFrontend/src/pages/Settings/Settings.jsx
@@ -6,7 +6,7 @@ import GeneralSettings from './GeneralSettings';
 import PaymentSettings from './PaymentSettings';
 import InvoiceSettings from './InvoiceSettings';
 
-const RightMenu = ({ tabOnChange }) => {
+const RightMenu = ({ handleTabChange }) => {
   const menuItems = [
     { key: 'generalSettings', label: 'generalSettings' },
     { key: 'paymentSettings', label: 'paymentSettings' },
@@ -16,7 +16,7 @@ const RightMenu = ({ tabOnChange }) => {
   return (
     <Menu mode="inline" defaultSelectedKeys={['generalSettings']}>
       {menuItems.map((item) => (
-        <Menu.Item key={item.key} onClick={() => tabOnChange(item.key)}>
+        <Menu.Item key={item.key} onClick={() => handleTabChange(item.key)}>
           {item.label}
         </Menu.Item>
       ))}
@@ -24,24 +24,25 @@ const RightMenu = ({ tabOnChange }) => {
   );
 };
 
-const Visibility = ({ isVisible, children }) => {
+const Visibility = ({ isVisible = false, children }) => {
   const show = isVisible ? { display: 'block', opacity: 1 } : { display: 'none', opacity: 0 };
   return <div style={show}>{children}</div>;
 };
 
 export default function Settings() {
-  const [state, setSate] = useState([
+  const initialState = [
     { tab: 'generalSettings', isActive: true },
     { tab: 'paymentSettings', isActive: false },
     { tab: 'invoiceSettings', isActive: false },
-  ]);
+  ];
+  const [state, setState] = useState(initialState);
 
   const isActive = (tab) => {
     const currentTab = state.find((x) => x.tab === tab);
     return currentTab && currentTab.isActive ? true : false;
   };
 
-  const tabOnChange = (tab) => {
+  const handleTabChange = (tab) => {
     const myNewList = [...state];
     myNewList.map((a) => {
       if (a.tab === tab) {
@@ -51,15 +52,16 @@ export default function Settings() {
       }
     });
 
-    setSate(myNewList);
+    setState(myNewList);
   };
+
   return (
     <SettingsLayout
       topCardContent="Generals Settings"
       topCardTitle="Settings"
-      bottomCardContent={<RightMenu tabOnChange={tabOnChange} />}
+      bottomCardContent={<RightMenu handleTabChange={handleTabChange} />}
     >
-      <Visibility isVisible={isActive('generalSettings')}>
+      <Visibility isVisible={isActive('generalSettings') || true}>
         <GeneralSettings />
       </Visibility>
       <Visibility isVisible={isActive('paymentSettings')}>
