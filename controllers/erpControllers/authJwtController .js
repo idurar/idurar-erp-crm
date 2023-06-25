32c2d608ec674af8c239beb18ae9321f6b99de73
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-06-25 18:01:00 +0100

    cookies bugs should be fixed

--- controllers/erpControllers/authJwtController .js
+++ controllers/erpControllers/authJwtController .js
@@ -42,7 +42,7 @@ exports.login = async (req, res) => {
         id: admin._id,
       },
       process.env.JWT_SECRET,
-      { expiresIn: '72h' }
+      { expiresIn: req.body.remember ? 365 * 24 + 'h' : '24h' }
     );
 
     const result = await Admin.findOneAndUpdate(
@@ -53,25 +53,26 @@ exports.login = async (req, res) => {
       }
     ).exec();
 
-    res.cookie('token', token, {
-      maxAge: req.body.remember ? 72 * 60 * 60 * 1000 : 60 * 60 * 1000, // Cookie expires after 30 days
-      sameSite: 'none',
-      httpOnly: true,
-      secure: true,
-    });
-
-    res.json({
-      success: true,
-      result: {
-        token,
-        admin: {
-          id: result._id,
-          name: result.name,
-          isLoggedIn: result.isLoggedIn,
+    res
+      .status(200)
+      .cookie('token', token, {
+        maxAge: req.body.remember ? 365 * 24 * 60 * 60 * 1000 : null, // Cookie expires after 30 days
+        sameSite: 'none',
+        httpOnly: true,
+        secure: true,
+      })
+      .json({
+        success: true,
+        result: {
+          token,
+          admin: {
+            id: result._id,
+            name: result.name,
+            isLoggedIn: result.isLoggedIn,
+          },
         },
-      },
-      message: 'Successfully login admin',
-    });
+        message: 'Successfully login admin',
+      });
   } catch (err) {
     res.status(500).json({ success: false, result: null, message: err.message, error: err });
   }
