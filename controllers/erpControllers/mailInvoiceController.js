bfa616ee8d95636f93773ecd87670c8250ad4163
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-08-02 19:23:59 +0100

    feat: update async support and improve errors

--- controllers/erpControllers/mailInvoiceController.js
+++ controllers/erpControllers/mailInvoiceController.js
@@ -1,37 +1,57 @@
 const fs = require('fs');
+const custom = require('../corsControllers/custom');
 const mongoose = require('mongoose');
 const InvoiceModel = mongoose.model('Invoice');
 const ClientModel = mongoose.model('Client');
 const ObjectId = mongoose.Types.ObjectId;
 const { Resend } = require('resend');
+const path = require('path');
 
 module.exports = sendMail = async (req, res) => {
   const { id } = req.body;
 
+  // Throw error if no id
+  if (!id) {
+    throw { name: 'ValidationError' };
+  }
+
   try {
-    // Throw error if no id
-    if (!id) {
+    const result = await InvoiceModel.findById(ObjectId(id)).exec();
+
+    // Throw error if no result
+    if (!result) {
       throw { name: 'ValidationError' };
     }
 
-    const result = await InvoiceModel.findById(ObjectId(id)).exec();
-
     // Continue process if result is returned
-    if (result) {
-      const { pdfPath, client } = result;
-      const fileLocation = `public/download/invoice/${pdfPath}`;
-      const { email, managerName } = await ClientModel.findById(client).exec();
+    const { client } = result;
+    const { email, managerName } = await ClientModel.findById(client).exec();
 
-      // Send the mail using the details gotten from the client
-      const { id: mailId } = await sendViaApi(email, managerName, fileLocation);
+    await custom
+      .generatePdf(
+        'Invoice',
+        { filename: 'invoice', format: 'A4' },
+        result,
+        async (fileLocation) => {
+          // Send the mail using the details gotten from the client
+          const { id: mailId } = await sendViaApi(email, managerName, fileLocation);
 
-      // Returning successfull response
-      return res.status(200).json({
-        success: true,
-        result: mailId,
-        message: `Successfully sent invoice ${id} to ${email}`,
+          // Returning successfull response
+          return res.status(200).json({
+            success: true,
+            result: mailId,
+            message: `Successfully sent invoice ${id} to ${email}`,
+          });
+        }
+      )
+      .catch((err) => {
+        return res.status(500).json({
+          success: false,
+          result: null,
+          error: err,
+          message: 'Oops there is an Error',
+        });
       });
-    }
   } catch (err) {
     // If err is thrown by Mongoose due to required validations
     if (err.name == 'ValidationError') {
@@ -40,6 +60,14 @@ module.exports = sendMail = async (req, res) => {
         result: null,
         error: err,
         message: 'Required fields are not supplied',
+      });
+    } else if (err.name == 'BSONTypeError') {
+      // If err is thrown by Mongoose due to invalid ID
+      return res.status(400).json({
+        success: false,
+        result: null,
+        error: err,
+        message: 'Invalid ID',
       });
     } else {
       // Server Error
@@ -54,14 +82,15 @@ module.exports = sendMail = async (req, res) => {
 };
 
 const sendViaApi = async (email, name, filePath) => {
+  const absolutePath = path.normalize(filePath);
   const resend = new Resend(process.env.RESEND_API);
 
   // Read the file to be attatched
-  const attatchedFile = fs.readFileSync(filePath);
+  const attatchedFile = fs.readFileSync(absolutePath);
 
   // Send the mail using the send method
   const data = await resend.emails.send({
-    from: 'idurar@onfranciis.dev',
+    from: 'Idurar@onfranciis.dev',
     to: email,
     subject: 'Invoice From Idurar',
     attachments: [
@@ -71,7 +100,7 @@ const sendViaApi = async (email, name, filePath) => {
       },
     ],
     html: `
-    <p>Hello ${name}, <br>
+    <p>Hello ${name}, <br> <br>
     Here's the invoice you requested at <br>
         <strong>${new Date()}</strong>
     </p>
