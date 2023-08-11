3270f321855670242d7175f33e38c2959f047ae4
Elham-EN <ilhamrezaie@gmail.com>
2023-08-11 21:57:42 +1000

    created quoteToInvoice converter api

--- controllers/erpControllers/quoteController.js
+++ controllers/erpControllers/quoteController.js
@@ -140,6 +140,78 @@ methods.update = async (req, res) => {
   }
 };
 
+const InvoiceModel = mongoose.model('Invoice');
+
+methods.convertQuoteToInvoice = async (req, res) => {
+  try {
+    const quoteId = req.params.id; // Assuming the quote ID is passed in the URL
+
+    // Fetch the quote from the database
+    const quote = await Model.findById(quoteId);
+    if (!quote) {
+      return res.status(404).json({
+        success: false,
+        result: null,
+        message: 'Quote not found',
+      });
+    }
+
+    // If the quote is already converted, prevent creating another invoice
+    if (quote.converted) {
+      return res.status(400).json({
+        success: false,
+        result: null,
+        message: 'Quote is already converted to an invoice.',
+      });
+    }
+
+    // Convert the quote details to invoice details
+    const invoiceData = {
+      number: quote.number,
+      year: quote.year,
+      date: quote.date,
+      expiredDate: quote.expiredDate,
+      client: quote.client,
+      items: quote.items.map((item) => ({
+        itemName: item.itemName,
+        description: item.description,
+        quantity: item.quantity,
+        price: item.price,
+        total: item.total,
+      })),
+      taxRate: quote.taxRate,
+      subTotal: quote.subTotal,
+      taxTotal: quote.taxTotal,
+      total: quote.total,
+      credit: quote.credit,
+      discount: quote.discount,
+      note: quote.note,
+    };
+
+    // Create the invoice document
+    const invoice = await new InvoiceModel(invoiceData).save();
+
+    // Mark the quote as converted
+    quote.converted = true;
+    await quote.save();
+
+    // Return the created invoice
+    return res.status(200).json({
+      success: true,
+      result: invoice,
+      message: 'Successfully converted quote to invoice',
+    });
+  } catch (err) {
+    // Error handling similar to your existing code
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      result: null,
+      message: 'Oops there is an Error',
+    });
+  }
+};
+
 // methods.update = async (req, res) => {
 //   try {
 //     const { id } = req.params;
