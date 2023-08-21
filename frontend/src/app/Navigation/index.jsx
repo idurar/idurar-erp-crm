2262ca191b1ee42cb033bd849de4b35d88768f2d
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-21 15:42:11 +0530

    fix:added a constant array for sidebar menu and submenu items in navigation

--- frontend/src/app/Navigation/index.jsx
+++ frontend/src/app/Navigation/index.jsx
@@ -19,6 +19,22 @@ import {
   MenuOutlined,
 } from '@ant-design/icons';
 
+const SIDEBAR_MENU = [
+  { key: '/', icon: <DashboardOutlined />, title: 'Dashboard' },
+  { key: '/customer', icon: <CustomerServiceOutlined />, title: 'Customer' },
+  { key: '/invoice', icon: <FileTextOutlined />, title: 'Invoice' },
+  { key: '/quote', icon: <FileSyncOutlined />, title: 'Quote' },
+  { key: '/payment/invoice', icon: <CreditCardOutlined />, title: 'Payment Invoice' },
+  { key: '/employee', icon: <UserOutlined />, title: 'Employee' },
+  { key: '/admin', icon: <TeamOutlined />, title: 'Admin' },
+];
+
+const SETTINGS_SUBMENU = [
+  { key: '/settings', title: 'General Settings' },
+  { key: '/payment/mode', title: 'Payment Mode' },
+  { key: '/role', title: 'Role' },
+];
+
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -81,47 +97,19 @@ function Sidebar({ collapsible }) {
           )}
         </div>
         <Menu mode="inline" selectedKeys={[currentPath]}>
-          <Menu.Item key={'/'} icon={<DashboardOutlined />}>
-            <Link to={'/'} />
-            Dashboard
-          </Menu.Item>
-          <Menu.Item key={'/customer'} icon={<CustomerServiceOutlined />}>
-            <Link to={'/customer'} />
-            Customer
-          </Menu.Item>
-          <Menu.Item key={'/invoice'} icon={<FileTextOutlined />}>
-            <Link to={'/invoice'} />
-            Invoice
-          </Menu.Item>
-          <Menu.Item key={'/quote'} icon={<FileSyncOutlined />}>
-            <Link to={'/quote'} />
-            Quote
-          </Menu.Item>
-          <Menu.Item key={'/payment/invoice'} icon={<CreditCardOutlined />}>
-            <Link to={'/payment/invoice'} />
-            Payment Invoice
-          </Menu.Item>
-          <Menu.Item key={'/employee'} icon={<UserOutlined />}>
-            <Link to={'/employee'} />
-            Employee
-          </Menu.Item>
-          <Menu.Item key={'/admin'} icon={<TeamOutlined />}>
-            <Link to={'/admin'} />
-            Admin
-          </Menu.Item>
+          {SIDEBAR_MENU.map((menuItem) => (
+            <Menu.Item key={menuItem.key} icon={menuItem.icon}>
+              <Link to={menuItem.key} />
+              {menuItem.title}
+            </Menu.Item>
+          ))}
           <SubMenu key={'Settings'} icon={<SettingOutlined />} title={'Settings'}>
-            <Menu.Item key={'/settings'}>
-              <Link to={'/settings'} />
-              General Settings
-            </Menu.Item>
-            <Menu.Item key={'/payment/mode'}>
-              <Link to={'/payment/mode'} />
-              Payment Mode
-            </Menu.Item>
-            <Menu.Item key={'/role'}>
-              <Link to={'/role'} />
-              Role
-            </Menu.Item>
+            {SETTINGS_SUBMENU.map((menuItem) => (
+              <Menu.Item key={menuItem.key}>
+                <Link to={menuItem.key} />
+                {menuItem.title}
+              </Menu.Item>
+            ))}
           </SubMenu>
         </Menu>
       </Sider>
