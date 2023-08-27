018e3fb516eb0e1cb3be8805dde1719fec68768f
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-08-27 14:55:08 +0100

    add env file REACT_APP_BACKEND_SERVER

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://dev-server.idurarapp.com/api/'
+    ? process.env.REACT_APP_BACKEND_SERVER + '/api/'
     : 'http://localhost:8888/api/';
 export const DOWNLOAD_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://dev-server.idurarapp.com/download/'
+    ? process.env.REACT_APP_BACKEND_SERVER + '/download/'
     : 'http://localhost:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
