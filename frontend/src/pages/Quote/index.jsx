637122545229f9d663dcd6fb2c6e2edd0d336942
onfranciis <onukwuf@gmail.com>
2023-09-03 15:52:43 +0100

    feat: refactor quote

--- frontend/src/pages/Quote/index.jsx
+++ frontend/src/pages/Quote/index.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import dayjs from 'dayjs';
 import { Tag } from 'antd';
 
 import QuoteModule from '@/modules/QuoteModule';
 import { useMoney } from '@/settings';
+import configPage from './config';
 
 export default function Quote() {
   const { moneyRowFormatter } = useMoney();
 
-  const entity = 'quote';
   const searchConfig = {
     displayLabels: ['name', 'surname'],
     searchFields: 'name,surname,birthday',
@@ -67,23 +66,8 @@ export default function Quote() {
     },
   ];
 
-  const PANEL_TITLE = 'quote';
-  const dataTableTitle = 'quotes Lists';
-  const ADD_NEW_ENTITY = 'Add new quote';
-  const DATATABLE_TITLE = 'quotes List';
-  const ENTITY_NAME = 'quote';
-  const CREATE_ENTITY = 'Save quote';
-  const UPDATE_ENTITY = 'Update quote';
-
   const config = {
-    entity,
-    PANEL_TITLE,
-    dataTableTitle,
-    ENTITY_NAME,
-    CREATE_ENTITY,
-    ADD_NEW_ENTITY,
-    UPDATE_ENTITY,
-    DATATABLE_TITLE,
+    ...configPage,
     dataTableColumns,
     searchConfig,
     entityDisplayLabels,
