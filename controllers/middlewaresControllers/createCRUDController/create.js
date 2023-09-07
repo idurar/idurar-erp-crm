0e90539ce1ef1bd06373c1949cd67dd728ff7d88
Gaurav Kumar <97530398+Gaurav01Kumar@users.noreply.github.com>
2023-09-07 18:15:06 +0530

    issues/issues/fix-authentication-issue

--- controllers/middlewaresControllers/createCRUDController/create.js
+++ controllers/middlewaresControllers/createCRUDController/create.js
@@ -1,9 +1,7 @@
 const create = async (Model, req, res) => {
   try {
     // Creating a new document in the collection
-
-    const result = await new Model(req.body).save();
-
+     const result = await new Model(req.body).save();
     // Returning successfull response
     return res.status(200).json({
       success: true,
