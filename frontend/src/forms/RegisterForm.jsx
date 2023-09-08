0a586b14db3e464fe2200bb866e154b7eadeda75
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-09-08 10:06:00 +0530

    chore: changed error message

--- frontend/src/forms/RegisterForm.jsx
+++ frontend/src/forms/RegisterForm.jsx
@@ -67,7 +67,7 @@ export default function RegisterForm() {
               if (!value || getFieldValue('password') === value) {
                 return Promise.resolve();
               }
-              return Promise.reject(new Error('The new password that you entered do not match!'));
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
             },
           }),
         ]}
