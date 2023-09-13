a97e4d8ea2e4254c41d358316b0d0cac1beaea4e
Manzi Eric <50232091+manziEric@users.noreply.github.com>
2023-09-13 21:36:11 +0200

    add profile info of current user

--- frontend/src/components/Profile/index.jsx
+++ frontend/src/components/Profile/index.jsx
@@ -4,6 +4,9 @@ import { useProfileContext } from '@/context/profileContext';
 import AdminInfo from './AdminInfo';
 import UpdateAdmin from './UpdateAdmin';
 import PasswordModal from './PasswordModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrentAdmin } from '@/redux/auth/selectors';
+import { crud } from '@/redux/crud/actions';
 
 const Visibility = ({ isVisible, children }) => {
   const show = isVisible ? { display: 'block', opacity: 1 } : { display: 'none', opacity: 0 };
@@ -13,6 +16,12 @@ const Visibility = ({ isVisible, children }) => {
 export default function Profile({ config }) {
   const { state } = useProfileContext();
   const { update, read, passwordModal } = state;
+  const { id } = useSelector(selectCurrentAdmin);
+  // using the crud redux to fetch and update the admin
+  const entity = 'admin';
+  const dispatch = useDispatch();
+  dispatch(crud.read({ entity, id }));
+  config = { ...config, id };
 
   return (
     <>
