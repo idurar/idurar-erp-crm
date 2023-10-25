5a7bff6dce4ef2360169bb85388a02ffaaa1e1e4
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-10-25 20:34:54 +0100

    🚚 Fix not found download pdf file

--- backend/routes/coreRoutes/coreDownloadRouter.js
+++ backend/routes/coreRoutes/coreDownloadRouter.js
@@ -1,24 +1,36 @@
 const downloadPdf = require('@/handlers/downloadHandler/downloadPdf');
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 const { hasPermission } = require('@/middlewares/permission');
 
-router.route('/:subPath/:directory/:id').get(function (req, res) {
-  const { subPath, directory, id } = req.params;
+// router.route('/:directory/:file').get(function (req, res) {
+//   const { directory, file } = req.params;
 
-  // Handle the /payment/* route
-  if (subPath == 'payment' && directory == 'invoice') {
-    downloadPdf(req, res, { directory: 'Payment', id });
-  } else {
-    downloadPdf(req, res, { directory, id });
-  }
-});
+//   // Handle the /payment/* route
 
-// router.route('/:directory/:id').get(function (req, res) {
-//   const { directory, id } = req.params;
+//   const options = {
+//     root: path.join(__dirname, `../../public/download/${directory}`),
+//     dotfiles: 'deny',
+//     headers: {
+//       'Content-type': 'application/pdf',
+//       'Content-disposition': 'inline; filename="' + file + '"',
+//     },
+//   };
 
-//   downloadPdf(req, res, { directory, id });
+//   res.status(200).sendFile(file, options, function (err) {
+//     if (err) {
+//       const id = file.slice(directory.length + 1).slice(0, -4); // extract id from file name
+//       downloadPdf(req, res, { directory, id });
+//     }
+//   });
 // });
+
+router.route('/:directory/:file').get(function (req, res) {
+  const { directory, file } = req.params;
+
+  const id = file.slice(directory.length + 1).slice(0, -4); // extract id from file name
+  downloadPdf(req, res, { directory, id });
+});
 
 module.exports = router;
