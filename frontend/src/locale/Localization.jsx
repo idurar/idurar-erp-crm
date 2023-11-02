ad0f807718ba90ab48c66970e3d5266e6f20f837
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-11-02 03:11:06 +0100

    👌 IMPROVE: Refactor localisation

--- frontend/src/locale/Localization.jsx
+++ frontend/src/locale/Localization.jsx
@@ -1,48 +1,26 @@
 import { useState, useEffect } from 'react';
 
-import enUS from 'antd/es/locale/en_US';
-import zhCN from 'antd/es/locale/zh_CN';
-import frFR from 'antd/es/locale/fr_FR';
-import arEG from 'antd/es/locale/ar_EG';
-import roRO from 'antd/es/locale/ro_RO';
-import itIT from 'antd/es/locale/it_IT';
-
 import { ConfigProvider } from 'antd';
 
 import { useSelector } from 'react-redux';
 
-import { selectLangCode } from '@/redux/translate/selectors';
+import { selectLangState } from '@/redux/translate/selectors';
+
+const importLangFile = async () => {
+  return await import('./antdLocale');
+};
 
 export default function Localization({ children }) {
-  const langCode = useSelector(selectLangCode);
+  const { langCode, langDirection } = useSelector(selectLangState);
 
-  const [locale, setLocal] = useState(enUS);
-  const [direction, setDirection] = useState('ltr');
+  const [locale, setLocal] = useState();
+  const [direction, setDirection] = useState();
 
   useEffect(() => {
-    if (langCode === 'fr_fr') {
-      setDirection('ltr');
-      setLocal(frFR);
-    } else if (langCode === 'zh_cn') {
-      setDirection('ltr');
-      setLocal(zhCN);
-    } else if (langCode === 'ro_ro') {
-      setDirection('ltr');
-      setLocal(roRO);
-    } else if (langCode === 'ar_eg') {
-      setDirection('rtl');
-      setLocal(arEG);
-    } else if (langCode === 'it_it') {
-      setDirection('ltr');
-      setLocal(itIT);
-    } else {
-      setDirection('ltr');
-      setLocal(enUS);
-    }
-
-    return () => {
-      return false;
-    };
+    const antdLocale = importLangFile();
+    const lang = antdLocale[langCode];
+    setDirection(langDirection);
+    setLocal(lang);
   }, [langCode]);
 
   return (
--- frontend/src/redux/translate/actions.js
+++ frontend/src/redux/translate/actions.js
@@ -1,12 +1,7 @@
 import * as actionTypes from './types';
-import { request } from '@/request';
 
-import en_us from '@/locale/translation/en_us';
-import fr_fr from '@/locale/translation/fr_fr';
-import zh_cn from '@/locale/translation/zh_cn';
-import ar_eg from '@/locale/translation/ar_eg';
-import it_it from '@/locale/translation/it_it';
-import ro_ro from '@/locale/translation/ro_ro';
+import translation from '@/locale/translation';
+import languages from '@/locale/languages';
 
 export const translateAction = {
   resetState: () => (dispatch) => {
@@ -19,24 +14,11 @@ export const translateAction = {
       type: actionTypes.REQUEST_LOADING,
     });
 
-    let data = null;
-
-    if (value === 'zh_cn') {
-      data = zh_cn;
-    } else if (value === 'fr_fr') {
-      data = fr_fr;
-    } else if (value === 'ro_ro') {
-      data = ro_ro;
-    } else if (value === 'ar_eg') {
-      data = ar_eg;
-    } else if (value === 'it_it') {
-      data = it_it;
-    } else {
-      data = en_us;
-    }
-
+    let data = translation[value];
+    const isRtl = languages.find((l) => l.value === value).isRtl || false;
     const LANG_STATE = {
       result: data,
+      isRtl: isRtl,
       langCode: value,
       isLoading: false,
       isSuccess: false,
