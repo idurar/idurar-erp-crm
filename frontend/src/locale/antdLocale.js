d3cddf617261e2c77e12cff7226829bba9e0058f
Giovanni Vella <giovanni.vella98@gmail.com>
2023-11-03 13:40:52 +0100

    added German language
    solved #652

--- frontend/src/locale/antdLocale.js
+++ frontend/src/locale/antdLocale.js
@@ -4,6 +4,7 @@ import zhCN from 'antd/es/locale/zh_CN';
 import roRO from 'antd/es/locale/ro_RO';
 import itIT from 'antd/es/locale/it_IT';
 import hiIN from 'antd/es/locale/hi_IN';
+import deDE from 'antd/es/locale/de_DE';
 const antdLocale = {
   zh_cn: zhCN,
   fr_fr: frFR,
@@ -11,6 +12,7 @@ const antdLocale = {
   ro_ro: roRO,
   it_it: itIT,
   hi_in: hiIN,
+  de_de: deDE,
 };
 
 export default antdLocale;
--- frontend/src/locale/translation/index.js
+++ frontend/src/locale/translation/index.js
@@ -5,6 +5,7 @@ import ar_eg from './ar_eg';
 import it_it from './it_it';
 import ro_ro from './ro_ro';
 import hi_in from './hi_in';
+import de_de from './de_de';
 const languages = {
   ar_eg,
   en_us,
@@ -13,6 +14,7 @@ const languages = {
   it_it,
   ro_ro,
   hi_in,
+  de_de,
 };
 
 export default languages;
