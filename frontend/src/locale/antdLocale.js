8d506fef4ec3fd4f4170cf4463e17c7997118895
sandeep <sandeepkrpatel2002@gmail.com>
2023-11-03 21:59:31 +0530

    Dutch_lan_added

--- frontend/src/locale/antdLocale.js
+++ frontend/src/locale/antdLocale.js
@@ -3,13 +3,14 @@ import arEG from 'antd/es/locale/ar_EG';
 import zhCN from 'antd/es/locale/zh_CN';
 import roRO from 'antd/es/locale/ro_RO';
 import itIT from 'antd/es/locale/it_IT';
-
+import nlNL from 'antd/es/locale/nl_NL';
 const antdLocale = {
   zh_cn: zhCN,
   fr_fr: frFR,
   ar_eg: arEG,
   ro_ro: roRO,
   it_it: itIT,
+  nl_nl: nlNL,
 };
 
 export default antdLocale;
--- frontend/src/locale/translation/index.js
+++ frontend/src/locale/translation/index.js
@@ -4,7 +4,7 @@ import zh_cn from './zh_cn';
 import ar_eg from './ar_eg';
 import it_it from './it_it';
 import ro_ro from './ro_ro';
-
+import nl_nl from './nl_nl';
 const languages = {
   ar_eg,
   en_us,
@@ -12,6 +12,7 @@ const languages = {
   zh_cn,
   it_it,
   ro_ro,
+  nl_nl,
 };
 
 export default languages;
