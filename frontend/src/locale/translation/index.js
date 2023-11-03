b7c306b88530db622187135e2c61cd144b408ff0
Shruti Chaudhary <shchaudhary1104@gmail.com>
2023-11-03 21:15:45 +0700

    feat: added thai language support

--- frontend/src/locale/translation/index.js
+++ frontend/src/locale/translation/index.js
@@ -5,6 +5,7 @@ import ar_eg from './ar_eg';
 import it_it from './it_it';
 import ro_ro from './ro_ro';
 import hi_in from './hi_in';
+import th_th from './th_th';
 const languages = {
   ar_eg,
   en_us,
@@ -13,6 +14,7 @@ const languages = {
   it_it,
   ro_ro,
   hi_in,
+  th_th
 };
 
 export default languages;
