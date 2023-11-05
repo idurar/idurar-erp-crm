aadcb0c3dc5f3d7d14ad4f378c3c6ba60849e8c5
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-11-05 11:44:42 +0100

    update package app version

--- frontend/src/locale/languages.js
+++ frontend/src/locale/languages.js
@@ -1,5 +1,6 @@
 const languages = [
   { icon: '🇩🇿 ', label: 'Arabic', value: 'ar_eg', isRtl: true },
+  { icon: '🇧🇩 ', label: 'Bengali', value: 'bn_bd' },
   { icon: '🇧🇬 ', label: 'Bulgarian', value: 'bg_bg' },
   { icon: '🇨🇳 ', label: 'Chinese', value: 'zh_cn' },
   { icon: '🇩🇰 ', label: 'Danish', value: 'da_dk' },
@@ -16,7 +17,7 @@ const languages = [
   { icon: '🇷🇴 ', label: 'Romanian', value: 'ro_ro' },
   { icon: '🇷🇺 ', label: 'Russian', value: 'ru_ru' },
   { icon: '🇸🇰 ', label: 'Slovak', value: 'sk_sk' },
-  { icon: '🇸🇮 ', label: 'Slovenian', value: 'sl_si'},
+  { icon: '🇸🇮 ', label: 'Slovenian', value: 'sl_si' },
   { icon: '🇪🇸 ', label: 'Spanish', value: 'es_es' },
   { icon: '🇸🇪 ', label: 'Swedish', value: 'sv_se' },
   { icon: '🇹🇭 ', label: 'Thai', value: 'th_th' },
@@ -25,7 +26,6 @@ const languages = [
   { icon: '🇵🇰 ', label: 'Urdu', value: 'ur_pk', isRtl: true },
   { icon: '🇻🇳 ', label: 'Vietnamese', value: 'vi_vn' },
   { icon: '🇦🇱 ', label: 'Albanian (coming soon)', value: 'sq_al', disabled: true },
-  { icon: '🇧🇩 ', label: 'Bengali', value: 'bn_bd'},
   { icon: '🇪🇦 ', label: 'Catalonian (coming soon)', value: 'ca_es', disabled: true },
   { icon: '🇭🇷 ', label: 'Croatian (coming soon)', value: 'hr_hr', disabled: true },
   { icon: '🇨🇿 ', label: 'Czech (coming soon)', value: 'cs_cz', disabled: true },
