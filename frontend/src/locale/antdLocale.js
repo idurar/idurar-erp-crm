ecb6989695138739613d192da698cd5a95af22a9
Habeeb Moosa <habeebmoosadev@gmail.com>
2023-11-06 00:46:56 +0530

    Malay language is added

--- frontend/src/locale/antdLocale.js
+++ frontend/src/locale/antdLocale.js
@@ -20,6 +20,7 @@ import daDK from 'antd/es/locale/da_DK';
 import bgBG from 'antd/es/locale/bg_BG';
 import svSE from 'antd/es/locale/sv_SE';
 import plPL from 'antd/es/locale/pl_PL';
+import msMY from 'antd/es/locale/ms_MY';
 
 const antdLocale = {
   zh_cn: zhCN,
@@ -44,6 +45,7 @@ const antdLocale = {
   bg_bg: bgBG,
   sv_se: svSE,
   pl_pl: plPL,
+  ms_my: msMY,
 };
 
 export default antdLocale;
