1022e0e8817178f0d0897015d925a17328119bca
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-11-11 13:19:30 +0100

    update auth login with no-cors

--- frontend/src/auth/auth.service.js
+++ frontend/src/auth/auth.service.js
@@ -8,14 +8,14 @@ export const login = async ({ loginData }) => {
   try {
     const response = await fetch(API_BASE_URL + `login?timestamp=${new Date().getTime()}`, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
+      mode: 'no-cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cache
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: 'follow', // manual, *follow, error
+      redirect: 'manual', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(loginData), // body data type must match "Content-Type" header
     });
