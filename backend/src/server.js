fc99e035ca77b7ea84f2d4c2f992b9df358983dd
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-11-18 21:44:27 +0100

    remove cluster

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 // Make sure we are running node 7.6+
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
-if (major < 18) {
-  console.log('Please upgrade your node.js version at least 18.x.x or greater. 👌\n ');
+if (major < 16 || (major === 16 && minor <= 20)) {
+  console.log('Please upgrade your node.js version at least 16.20.2 or greater. 👌\n ');
   process.exit();
 }
 
@@ -15,7 +15,7 @@ require('dotenv').config({ path: '.env' });
 require('dotenv').config({ path: '.env.local' });
 
 mongoose.connect(process.env.DATABASE);
-
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (error) => {
   console.log(
     `1. 🔥 Common Error caused issue → : check your .env file first and add your mongodb url`
@@ -29,26 +29,9 @@ for (const filePath of modelsFiles) {
   require(path.resolve(filePath));
 }
 
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-
-if (cluster.isMaster) {
-  // Fork workers for each CPU
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    // If a worker dies, fork a new one to replace it
-    cluster.fork();
-  });
-} else {
-  const app = require('./app');
-  app.set('port', process.env.PORT || 8888);
-  console.log('🚀 ~ file: server.js:34 ~ numCPUs:', numCPUs);
-  const server = app.listen(app.get('port'), () => {
-    console.log(`Express running → On PORT : ${server.address().port}`);
-  });
-}
 // Start our app!
+const app = require('./app');
+app.set('port', process.env.PORT || 8888);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → On PORT : ${server.address().port}`);
+});
