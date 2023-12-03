5e066404b8ea17c96a72006c33ac8cd9cbe6f18f
Salah Eddine Lalami <136928179+salahlalami@users.noreply.github.com>
2023-12-03 01:10:27 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
+const helmet = require('helmet');
+const path = require('path');
 const cors = require('cors');
-const compression = require('compression');
 
 const cookieParser = require('cookie-parser');
+
+const helpers = require('./helpers');
 
 const coreAuthRouter = require('./routes/coreRoutes/coreAuth');
 const coreApiRouter = require('./routes/coreRoutes/coreApi');
@@ -17,18 +20,42 @@ const erpApiRouter = require('./routes/appRoutes/appApi');
 // create our Express app
 const app = express();
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
+// setting cors at one place for all the routes
+// putting cors as first in order to avoid unneccessary requests from unallowed origins
+
+// app.use(function (req, res, next) {
+//   if (req.url.includes('/api')) {
+//     cors(corsOptions)(req, res, next);
+//   } else {
+//     cors()(req, res, next);
+//   }
+// });
+
+// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+
+// Takes the raw requests and turns them into usable properties on req.body
+
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(compression());
+// pass variables to our templates + all requests
+
+app.use((req, res, next) => {
+  res.locals.h = helpers;
+  res.locals.admin = req.admin || null;
+  res.locals.currentPath = req.path;
+  next();
+});
+
 // Here our API Routes
 app.use('/api', coreAuthRouter);
 app.use('/api', isValidAdminToken, coreApiRouter);
@@ -38,6 +65,12 @@ app.use('/public', corePublicRouter);
 
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
 
 // production error handler
 app.use(errorHandlers.productionErrors);
