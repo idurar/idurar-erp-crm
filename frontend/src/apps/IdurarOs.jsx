30f426e966d67a168d113707a3a0d026fa8ef30f
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-12-10 00:02:10 +0100

    update

--- frontend/src/apps/IdurarOs.jsx
+++ frontend/src/apps/IdurarOs.jsx
@@ -22,6 +22,10 @@ const DefaultApp = () => (
 export default function IdurarOs() {
   const { isLoggedIn } = useSelector(selectAuth);
 
+  console.log(
+    '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+  );
+
   if (!isLoggedIn)
     return (
       <Localization>
