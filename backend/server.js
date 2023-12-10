5e3b9670fcfe426e7b075591defa804daaa3cdc7
Manzi Eric <50232091+manziEric@users.noreply.github.com>
2023-12-10 14:06:58 +0100

    Update vercel.json file and add require app back in server.js file

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,8 @@ glob.sync('./models/**/*.js').forEach(function (file) {
 });
 
 // Start our app!
+const app = require('./app');
+
 app.set('port', process.env.PORT || 8888);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → On PORT : ${server.address().port}`);
