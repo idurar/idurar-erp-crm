f992d055cf21db883f6661d06a9de69e0044e03e
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2023-12-10 21:43:50 +0100

    increase ai token

--- backend/src/aiTranslate/index.js
+++ backend/src/aiTranslate/index.js
@@ -63,7 +63,7 @@ async function translate(language, langObject) {
       },
     ],
     temperature: 0.5,
-    max_tokens: 7000,
+    max_tokens: 12000,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
