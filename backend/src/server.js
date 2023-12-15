f32f82e74555bb4fee20126dac951e2623a607de
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-12-15 19:17:42 +0100

    feat: update imports/exports in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 require('module-alias/register');
-const mongoose = require('mongoose');
-const { globSync } = require('glob');
-const path = require('path');
+import mongoose from 'mongoose';
+import { globSync } from 'glob';
+import { resolve } from 'path';
+const { connect, connection } = mongoose;
 
 // Make sure we are running node 7.6+
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
@@ -11,12 +12,13 @@ if (major < 20) {
 }
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: '.env' });
-require('dotenv').config({ path: '.env.local' });
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local' });
 
-mongoose.connect(process.env.DATABASE);
+connect(process.env.DATABASE);
 
-mongoose.connection.on('error', (error) => {
+connection.on('error', (error) => {
   console.log(
     `1. 🔥 Common Error caused issue → : check your .env file first and add your mongodb url`
   );
@@ -26,11 +28,11 @@ mongoose.connection.on('error', (error) => {
 const modelsFiles = globSync('./src/models/**/*.js');
 
 for (const filePath of modelsFiles) {
-  require(path.resolve(filePath));
+  require(resolve(filePath));
 }
 
 // Start our app!
-const app = require('./app');
+import app from './app.js';
 app.set('port', process.env.PORT || 8888);
 const server = app.listen(app.get('port'), () => {
   console.log(`IDURAR App running → On PORT : ${server.address().port}`);
