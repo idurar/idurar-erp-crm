19b1ac6f6fd81693ded8bcbc71ad1b18b18b1daf
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-12-18 19:42:25 +0100

    feat: update imports/exports, var in summary.js

--- backend/src/controllers/appControllers/invoiceController/summary.js
+++ backend/src/controllers/appControllers/invoiceController/summary.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const moment = require('moment');
+import mongoose from 'mongoose';
+import moment from 'moment';
 
 const Model = mongoose.model('Invoice');
 
@@ -21,11 +21,9 @@ const summary = async (req, res) => {
   }
 
   const currentDate = moment();
-  let startDate = currentDate.clone().startOf(defaultType);
-  let endDate = currentDate.clone().endOf(defaultType);
-
+  const startDate = currentDate.clone().startOf(defaultType);
+  const endDate = currentDate.clone().endOf(defaultType);
   const statuses = ['draft', 'pending', 'overdue', 'paid', 'unpaid', 'partially'];
-
   const response = await Model.aggregate([
     {
       $match: {
@@ -203,4 +201,4 @@ const summary = async (req, res) => {
   });
 };
 
-module.exports = summary;
+export default summary;
