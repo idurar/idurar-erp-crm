72e1be7160b3fa0e60593d3da5f5db62ef9798f6
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-12-22 15:33:54 +0100

    feat: update imports/exports, str

--- backend/src/controllers/appControllers/leadController/read.js
+++ backend/src/controllers/appControllers/leadController/read.js
@@ -1,26 +1,26 @@
-const { migrate } = require('./migrate');
+import { migrate } from './migrate.js';
 
 const read = async (Model, req, res) => {
   // Find document by id
-  let result = await Model.findOne({ _id: req.params.id, removed: false }).exec();
+  let result = await Model.findOne({ _id: req.params.id, removed: false });
   // If no results found, return document not found
   if (!result) {
     return res.status(404).json({
       success: false,
       result: null,
-      message: 'No document found by this id: ' + req.params.id,
+      message: `No document found by this id: ${req.params.id}`,
     });
   } else {
-    // Return success resposne
+    // Return success response
 
     const migratedData = migrate(result);
 
     return res.status(200).json({
       success: true,
       result: migratedData,
-      message: 'we found this document by this id: ' + req.params.id,
+      message: `we found this document by this id: ${req.params.id}`,
     });
   }
 };
 
-module.exports = read;
+export default read;
