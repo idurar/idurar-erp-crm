ee2677c12094806b54fd15a62163bfd15c6fea6c
Lukasz Mroz <lukasz1mroz@gmail.com>
2024-01-04 16:28:32 +0100

    feat: update imports/exports, string syntax

--- backend/src/controllers/middlewaresControllers/createCRUDController/read.js
+++ backend/src/controllers/middlewaresControllers/createCRUDController/read.js
@@ -6,16 +6,16 @@ const read = async (Model, req, res) => {
     return res.status(404).json({
       success: false,
       result: null,
-      message: 'No document found by this id: ' + req.params.id,
+      message: `No document found by this id: ${req.params.id}`,
     });
   } else {
-    // Return success resposne
+    // Return success response
     return res.status(200).json({
       success: true,
       result,
-      message: 'we found this document by this id: ' + req.params.id,
+      message: `we found this document by this id: ${req.params.id}`,
     });
   }
 };
 
-module.exports = read;
+export default read;
