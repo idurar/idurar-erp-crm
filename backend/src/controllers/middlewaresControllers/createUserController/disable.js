239abcc6d33064869c31b74d492c0ad6c51a9bec
Lukasz Mroz <lukasz1mroz@gmail.com>
2024-01-04 16:29:42 +0100

    feat: update imports/exports

--- backend/src/controllers/middlewaresControllers/createUserController/disable.js
+++ backend/src/controllers/middlewaresControllers/createUserController/disable.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const remove = async (userModel, req, res) => {
   const User = mongoose.model(userModel);
@@ -41,4 +41,4 @@ const remove = async (userModel, req, res) => {
   }
 };
 
-module.exports = remove;
+export default remove;
