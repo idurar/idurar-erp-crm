382291e02352d9c6dba315a122913ac5034ac837
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2024-01-06 01:53:43 +0100

    send idurar offer after success registration

--- backend/src/controllers/middlewaresControllers/createAuthMiddleware/forgetPassword.js
+++ backend/src/controllers/middlewaresControllers/createAuthMiddleware/forgetPassword.js
@@ -57,7 +57,7 @@ const forgetPassword = async (req, res, { userModel }) => {
 
   const link = url + '/resetpassword/' + user._id + '/' + resetToken;
 
-  await sendMail({ email, name: user.name, link, idurar_app_email, type: 'resetpassword' });
+  await sendMail({ email, name: user.name, link, idurar_app_email, type: 'passwordVerfication' });
 
   return res.status(200).json({
     success: true,
--- backend/src/emailTemplate/emailVerfication.js
+++ backend/src/emailTemplate/emailVerfication.js
@@ -60,3 +60,34 @@ exports.passwordVerfication = ({
     </div>
     `;
 };
+
+exports.afterRegistrationSuccess = ({
+  title = 'Customize IDURAR ERP CRM or build your own SaaS',
+  name = '',
+}) => {
+  return `
+    <div>
+
+        <head data-id="__react-email-head">
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <title>${title}</title>
+        </head>
+        <div id="__react-email-preview" style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Customize IDURAR or build your own Saas<div> ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿</div>
+        </div>
+
+        <body data-id="__react-email-body">
+            <h2 data-id="react-email-heading">${title}</h2>
+            <hr data-id="react-email-hr" style="width:100%;border:none;border-top:1px solid #eaeaea" />
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Hello ${name},</p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">I would like to invite you to book a call if you need : </p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0"> * Customize or adding new features to IDURAR ERP CRM for just $ 30 per hour.</p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0"> * For $ 5000 : create your own custom SaaS solution based on IDURAR ERP CRM , With IDURAR SaaS license you can save a significant amount of money by opting for our pre-order price of $5000 instead of investing $250,000 in an uncertain developer team. This opportunity allows you to build a tailored SaaS platform that meets your specific business needs.</p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0"> Book a call here https://calendly.com/lalami/meeting /p>
+            <hr data-id="react-email-hr" style="width:100%;border:none;border-top:1px solid #eaeaea" />
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Best regards,</p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Salah Eddine Lalami</p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Founder @ IDURAR</p>
+        </body>
+    </div>
+    `;
+};
