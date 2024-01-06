1a1b59ba01d75b1113f6d9052ce2b2ee9cb0c75a
Lukasz Mroz <lukasz1mroz@gmail.com>
2024-01-06 12:20:22 +0100

    feat: update imports/exports, prop

--- backend/src/routes/appRoutes/appApi.js
+++ backend/src/routes/appRoutes/appApi.js
@@ -1,35 +1,33 @@
-const express = require('express');
-const { catchErrors } = require('@/handlers/errorHandlers');
+import express from 'express';
+import { catchErrors } from '#handlers/errorHandlers';
 const router = express.Router();
 
-const { hasPermission } = require('@/middlewares/permission');
-const appControllers = require('@/controllers/appControllers');
-const { routesList } = require('@/models/utils');
+import { hasPermission } from '#middlewares/permission';
+import appControllers from '#controllers/appControllers/index.js';
+import { routesList } from '#models/utils/index.js';
 
 const routerApp = (entity, controller) => {
-  router
-    .route(`/${entity}/create`)
-    .post(hasPermission('create'), catchErrors(controller['create']));
-  router.route(`/${entity}/read/:id`).get(hasPermission('read'), catchErrors(controller['read']));
+  router.route(`/${entity}/create`).post(hasPermission('create'), catchErrors(controller.create));
+  router.route(`/${entity}/read/:id`).get(hasPermission('read'), catchErrors(controller.read));
   router
     .route(`/${entity}/update/:id`)
-    .patch(hasPermission('update'), catchErrors(controller['update']));
+    .patch(hasPermission('update'), catchErrors(controller.update));
   router
     .route(`/${entity}/delete/:id`)
-    .delete(hasPermission('delete'), catchErrors(controller['delete']));
-  router.route(`/${entity}/search`).get(hasPermission('read'), catchErrors(controller['search']));
-  router.route(`/${entity}/list`).get(hasPermission('read'), catchErrors(controller['list']));
-  router.route(`/${entity}/filter`).get(hasPermission('read'), catchErrors(controller['filter']));
-  router.route(`/${entity}/summary`).get(hasPermission('read'), catchErrors(controller['summary']));
+    .delete(hasPermission('delete'), catchErrors(controller.delete));
+  router.route(`/${entity}/search`).get(hasPermission('read'), catchErrors(controller.search));
+  router.route(`/${entity}/list`).get(hasPermission('read'), catchErrors(controller.list));
+  router.route(`/${entity}/filter`).get(hasPermission('read'), catchErrors(controller.filter));
+  router.route(`/${entity}/summary`).get(hasPermission('read'), catchErrors(controller.summary));
 
   if (entity === 'invoice' || entity === 'quote') {
-    router.route(`/${entity}/mail`).post(hasPermission('update'), catchErrors(controller['mail']));
+    router.route(`/${entity}/mail`).post(hasPermission('update'), catchErrors(controller.mail));
   }
 
   if (entity === 'quote') {
     router
       .route(`/${entity}/convert/:id`)
-      .get(hasPermission('update'), catchErrors(controller['convert']));
+      .get(hasPermission('update'), catchErrors(controller.convert));
   }
 };
 
@@ -38,4 +36,4 @@ routesList.forEach(({ entity, controllerName }) => {
   routerApp(entity, controller);
 });
 
-module.exports = router;
+export default router;
