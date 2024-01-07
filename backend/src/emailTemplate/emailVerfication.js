2084cf9e53d410ffbbd105b9b9d94f2c4e414133
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2024-01-07 11:24:18 +0100

    add link to email template

--- backend/src/emailTemplate/emailVerfication.js
+++ backend/src/emailTemplate/emailVerfication.js
@@ -19,7 +19,7 @@ exports.emailVerfication = ({
             <hr data-id="react-email-hr" style="width:100%;border:none;border-top:1px solid #eaeaea" />
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Hello ${name},</p>
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Thank you for signing up for IDURAR ! Before we can activate your account, we kindly ask you to verify your email address by clicking on the link provided below:</p>
-            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">${link}</p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0"><a href="${link}">${link}</a></p>
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Thank you for choosing IDURAR. We look forward to having you as a valued user!</p>
             <hr data-id="react-email-hr" style="width:100%;border:none;border-top:1px solid #eaeaea" />
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Best regards,</p>
@@ -51,7 +51,7 @@ exports.passwordVerfication = ({
             <hr data-id="react-email-hr" style="width:100%;border:none;border-top:1px solid #eaeaea" />
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Hello ${name},</p>
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">We have received a request to reset the password for your account on IDURAR. To proceed with the password reset, please click on the link provided below:</p>
-            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">${link}</p>
+            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0"><a href="${link}">${link}</a></p>
             <hr data-id="react-email-hr" style="width:100%;border:none;border-top:1px solid #eaeaea" />
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Best regards,</p>
             <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0">Salah Eddine Lalami</p>
