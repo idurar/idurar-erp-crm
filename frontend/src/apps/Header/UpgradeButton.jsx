5ddbb640215f9dd997ccb0e6671e2fe15f8ac3c6
ines <inesmrad20@gmail.com>
2024-02-16 16:07:20 +0100

    updated translation file

--- frontend/src/apps/Header/UpgradeButton.jsx
+++ frontend/src/apps/Header/UpgradeButton.jsx
@@ -11,7 +11,7 @@ export default function UpgradeButton() {
   const Content = () => {
     return (
       <>
-        <p>{'Do you need help customizing this app ?'}</p>
+        <p>{translate('Do you need help customizing this app ?')}</p>
         <Button
           type="primary"
           onClick={() => {
