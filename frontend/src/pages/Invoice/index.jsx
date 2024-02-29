6bb813e6ab6071cf857751f95f0ed95855f8cf5e
shaik.rasool <shaik.web27@gmail.com>
2024-02-29 15:39:57 +0530

    Update index.jsx

--- frontend/src/pages/Invoice/index.jsx
+++ frontend/src/pages/Invoice/index.jsx
@@ -1,10 +1,10 @@
-import dayjs from 'dayjs';
-import { Tag } from 'antd';
-import useLanguage from '@/locale/useLanguage';
-import { tagColor } from '@/utils/statusTagColor';
+import { useDate, useMoney } from '@/settings';
 
-import { useMoney, useDate } from '@/settings';
 import InvoiceDataTableModule from '@/modules/InvoiceModule/InvoiceDataTableModule';
+import { Tag } from 'antd';
+import dayjs from 'dayjs';
+import { tagColor } from '@/utils/statusTagColor';
+import useLanguage from '@/locale/useLanguage';
 
 export default function Invoice() {
   const translate = useLanguage();
@@ -20,7 +20,7 @@ export default function Invoice() {
   const deleteModalLabels = ['number', 'client.name'];
   const dataTableColumns = [
     {
-      title: translate('Number'),
+      title: translate('Inv. No.'),
       dataIndex: 'number',
     },
     {
@@ -35,7 +35,7 @@ export default function Invoice() {
       },
     },
     {
-      title: translate('expired Date'),
+      title: translate('Due Date'),
       dataIndex: 'expiredDate',
       render: (date) => {
         return dayjs(date).format(dateFormat);
