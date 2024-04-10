0be0d3f1ba56e37a6988d23397c576374bf7356a
Onolax <meenamonu2002@gmail.com>
2024-04-10 18:34:00 +0530

    Update app.js
    Added some comments for clarity of middlewares

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,22 +2,23 @@ const express = require('express');
 
 const cors = require('cors');
 const compression = require('compression');
-
 const cookieParser = require('cookie-parser');
 
 const coreAuthRouter = require('./routes/coreRoutes/coreAuth');
 const coreApiRouter = require('./routes/coreRoutes/coreApi');
 const coreDownloadRouter = require('./routes/coreRoutes/coreDownloadRouter');
 const corePublicRouter = require('./routes/coreRoutes/corePublicRouter');
-const adminAuth = require('./controllers/coreControllers/adminAuth');
+const coreAdminAuth = require('./controllers/coreControllers/adminAuth');
 
 const errorHandlers = require('./handlers/errorHandlers');
 const erpApiRouter = require('./routes/appRoutes/appApi');
 
 const fileUpload = require('express-fileupload');
+
 // create our Express app
 const app = express();
 
+// enable CORS with specific options
 app.use(
   cors({
     origin: true,
@@ -25,20 +26,19 @@ app.use(
   })
 );
 
+// parsing our data
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(compression());
 
-// // default options
+// default options
 // app.use(fileUpload());
 
 // Here our API Routes
-
 app.use('/api', coreAuthRouter);
-app.use('/api', adminAuth.isValidAuthToken, coreApiRouter);
-app.use('/api', adminAuth.isValidAuthToken, erpApiRouter);
+app.use('/api', coreAdminAuth.isValidAuthToken, coreApiRouter);
+app.use('/api', coreAdminAuth.isValidAuthToken, erpApiRouter);
 app.use('/download', coreDownloadRouter);
 app.use('/public', corePublicRouter);
 
