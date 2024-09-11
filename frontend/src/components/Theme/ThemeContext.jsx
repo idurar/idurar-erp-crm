cda5f3cff7fd553b94a035006574d0233a6f107e
dakshmehta007 <mehtadaksh85@gmail.com>
2024-09-11 17:53:48 +0530

    Normalize all the line endings

---
+++ frontend/src/components/Theme/ThemeContext.jsx
@@ -0,0 +1,25 @@
+// src/ThemeContext.js
+import React, { createContext, useState, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
