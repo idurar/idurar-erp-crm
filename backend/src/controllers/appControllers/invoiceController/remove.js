d88351d2c70d80ffd38b0d72571881f1d21c8dab
Salah Eddine Lalami <lalami.sdn@gmail.com>
2024-09-28 22:33:32 +0100

    update backend server

--- backend/src/controllers/appControllers/invoiceController/remove.js
+++ backend/src/controllers/appControllers/invoiceController/remove.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
 
 const Model = mongoose.model('Invoice');
-const ModalPayment = mongoose.model('Payment');
+const ModelPayment = mongoose.model('Payment');
 
 const remove = async (req, res) => {
+  const deletedInvoice = await Model.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      removed: false,
+    },
+    {
+      $set: {
+        removed: true,
+      },
+    }
+  ).exec();
+
+  if (!deletedInvoice) {
+    return res.status(404).json({
+      success: false,
+      result: null,
+      message: 'Invoice not found',
+    });
+  }
+  const paymentsInvoices = await ModelPayment.updateMany(
+    { invoice: deletedInvoice._id },
+    { $set: { removed: true } }
+  );
   return res.status(200).json({
     success: true,
-    result: null,
-    message: 'Please Upgrade to Premium  Version to have full features',
+    result: deletedInvoice,
+    message: 'Invoice deleted successfully',
   });
 };
 
