8ffda8f7ea7769ae9a2162933cfe2e6ab8e92f6e
Salah Eddine Lalami <lalami.sdn@gmail.com>
2024-09-29 16:46:43 +0100

    update dashboard

--- frontend/src/apps/Header/UpgradeButton.jsx
+++ frontend/src/apps/Header/UpgradeButton.jsx
@@ -6,55 +6,8 @@ import { RocketOutlined } from '@ant-design/icons';
 
 import useLanguage from '@/locale/useLanguage';
 
-const SelfHostedPlan = () => {
-  const features = [
-    'Self-Hosted Premium Version',
-    'ulimited Users',
-    'Multi-Currency - ulimited currency',
-    'Multi-Branch - ulimited branch',
-    'Free 1 year update',
-    '24/7 priority support',
-  ];
-
-  return (
-    <List
-      size="large"
-      footer={
-        <Button
-          type="primary"
-          size="large"
-          block
-          onClick={() => {
-            window.open('https://cloud.idurarapp.com');
-          }}
-        >
-          Purchase Now
-        </Button>
-      }
-      // bordered
-      dataSource={features}
-      renderItem={(item) => <List.Item style={{ textAlign: 'center' }}>{item}</List.Item>}
-    />
-  );
-};
-
 export default function UpgradeButton() {
   const translate = useLanguage();
-  const Content = () => {
-    return (
-      <SelfHostedPlan />
-      //   <p>{translate('Do you need help on customize of this app')}</p>
-      //   <Button
-      //     type="primary"
-      //     onClick={() => {
-      //       window.open(`https://www.idurarapp.com/contact-us/`);
-      //     }}
-      //   >
-      //     {translate('Contact us')}
-      //   </Button>
-      // </>
-    );
-  };
 
   return (
     <Badge count={1} size="small">
@@ -64,8 +17,6 @@ export default function UpgradeButton() {
           float: 'right',
           marginTop: '5px',
           cursor: 'pointer',
-          // background: '#f56a00',
-          // boxShadow: '0 2px 0 #f56a22',
         }}
         icon={<RocketOutlined />}
         onClick={() => {
