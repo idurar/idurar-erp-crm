0d623e1753a40442571954053de9fbb5072a8417
DoMINAToR98 <98kartik.sharma@gmail.com>
2024-10-04 00:01:32 -0700

    fix for subPath path traversal

--- backend/src/routes/coreRoutes/corePublicRouter.js
+++ backend/src/routes/coreRoutes/corePublicRouter.js
@@ -8,9 +8,9 @@ const path = require('path');
 router.route('/:subPath/:directory/:file').get(function (req, res) {
   try {
     const { subPath, directory, file } = req.params;
-
+    sanitizedPath = path.normalize(subPath).replace(/^(\.\.[\/\\])+/, '');
     const options = {
-      root: path.join(__dirname, `../../public/${subPath}/${directory}`),
+      root: path.join(__dirname, `../../public/${sanitizedPath}/${directory}`),
     };
     const fileName = file;
     return res.sendFile(fileName, options, function (error) {
