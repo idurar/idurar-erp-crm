578283e0aec1faccc399d0f8e2a2ba15717c7d15
Salah <lalami.sdn@gmail.com>
2025-07-31 16:51:53 +0100

    update admin cred

--- backend/src/controllers/middlewaresControllers/createUserController/updateProfilePassword.js
+++ backend/src/controllers/middlewaresControllers/createUserController/updateProfilePassword.js
@@ -32,7 +32,7 @@ const updateProfilePassword = async (userModel, req, res) => {
     salt: salt,
   };
 
-  if (userProfile.email === 'admin@demo.com') {
+  if (userProfile.email === 'admin@admin.com') {
     return res.status(403).json({
       success: false,
       result: null,
