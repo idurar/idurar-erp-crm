1dbd185a07beeafa243a0ba9abb5ce3ec64f7c13
Tharukesh23 <Tharukesh29@gmail.com>
2025-10-09 19:05:23 +0530

    feat: add Google OAuth integration and AuthSuccess page

---
+++ frontend/src/pages/AuthSuccess.jsx
@@ -0,0 +1,19 @@
+﻿import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function AuthSuccess() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
+    if (token) {
+      localStorage.setItem("idurar_token", token);
+      history.replace("/dashboard");
+    } else {
+      history.replace("/login");
+    }
+  }, [history]);
+
+  return <div>Signing you in...</div>;
+}
