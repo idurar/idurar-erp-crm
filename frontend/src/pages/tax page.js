9073a58ab92df19e5fb7aaf1e8d597a2d2ce1e91
Archana487 <archanavp2024@gmail.com>
2025-10-10 08:57:36 +0530

    Remove nested repository

---
+++ frontend/src/pages/tax page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TaxPage = () => {
+  return (
+    <div>
+      <h1>Tax Page</h1>
+      <p>This is where you will manage taxes.</p>
+    </div>
+  );
+};
+
+export default TaxPage;
